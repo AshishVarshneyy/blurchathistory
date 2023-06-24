3e1ce319e8520077031f786847fb404e136b68f4
Ashish Varshney <er.ashish.av@gmail.com>
2023-06-25 01:46:13 +0530

    version 1 commit

---
+++ content.js
@@ -0,0 +1,4 @@
+// Add event listener to the chat history container
+document.addEventListener("DOMContentLoaded", function () {
+    const chatHistoryContainer = document.querySelector(".flex-1.text-ellipsis.max-h-5.overflow-hidden.break-all.relative");
+});
